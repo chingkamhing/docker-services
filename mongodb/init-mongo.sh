691ea5c1cf29afeae552e7b19d46bca68fcb380f
chingkamhing <kam_ching@hotmail.com>
2021-07-13 01:25:04 +0800

    Update for mongodb

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -3,7 +3,7 @@
 # MongoDB init script that create initial DB and user during first mongodb docker start up
 #
 
-if [ "$IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_DBNAME" ] && [ "$IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_USER" ] && [ "$IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_PASSWORD" ]; then
+if [ "$DATABASE_NAME" ] && [ "$DATABASE_USERNAME" ] && [ "$DATABASE_PASSWORD" ]; then
     echo 'Creating application user and db for authen service'
     mongo \
         --host localhost \
@@ -11,27 +11,5 @@ if [ "$IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_DBNAME" ] && [ "$IMS_SECURITY_SE
         -u ${MONGO_INITDB_ROOT_USERNAME} \
         -p ${MONGO_INITDB_ROOT_PASSWORD} \
         admin \
-        --eval "db.getSiblingDB('${IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_DBNAME}').createUser({user: '${IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_USER}', pwd: '${IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_PASSWORD}', roles:[{role:'dbOwner', db: '${IMS_SECURITY_SERVICE_APIGATEWAY_DATABASE_DBNAME}'}]});"
-fi
-
-if [ "$IMS_SECURITY_SERVICE_AUTHEN_DATABASE_DBNAME" ] && [ "$IMS_SECURITY_SERVICE_AUTHEN_DATABASE_USER" ] && [ "$IMS_SECURITY_SERVICE_AUTHEN_DATABASE_PASSWORD" ]; then
-    echo 'Creating application user and db for authen service'
-    mongo \
-        --host localhost \
-        --port 27017 \
-        -u ${MONGO_INITDB_ROOT_USERNAME} \
-        -p ${MONGO_INITDB_ROOT_PASSWORD} \
-        admin \
-        --eval "db.getSiblingDB('${IMS_SECURITY_SERVICE_AUTHEN_DATABASE_DBNAME}').createUser({user: '${IMS_SECURITY_SERVICE_AUTHEN_DATABASE_USER}', pwd: '${IMS_SECURITY_SERVICE_AUTHEN_DATABASE_PASSWORD}', roles:[{role:'dbOwner', db: '${IMS_SECURITY_SERVICE_AUTHEN_DATABASE_DBNAME}'}]});"
-fi
-
-if [ "$IMS_SECURITY_SERVICE_USER_DATABASE_DBNAME" ] && [ "$IMS_SECURITY_SERVICE_USER_DATABASE_USER" ] && [ "$IMS_SECURITY_SERVICE_USER_DATABASE_PASSWORD" ]; then
-    echo 'Creating application user and db for user service'
-    mongo \
-        --host localhost \
-        --port 27017 \
-        -u ${MONGO_INITDB_ROOT_USERNAME} \
-        -p ${MONGO_INITDB_ROOT_PASSWORD} \
-        admin \
-        --eval "db.getSiblingDB('${IMS_SECURITY_SERVICE_USER_DATABASE_DBNAME}').createUser({user: '${IMS_SECURITY_SERVICE_USER_DATABASE_USER}', pwd: '${IMS_SECURITY_SERVICE_USER_DATABASE_PASSWORD}', roles:[{role:'dbOwner', db: '${IMS_SECURITY_SERVICE_USER_DATABASE_DBNAME}'}]});"
+        --eval "db.getSiblingDB('${DATABASE_NAME}').createUser({user: '${DATABASE_USERNAME}', pwd: '${DATABASE_PASSWORD}', roles:[{role:'dbOwner', db: '${DATABASE_NAME}'}]});"
 fi
