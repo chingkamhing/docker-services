56f99387d23e935e3fc08a400b7834094c3395b5
chingkamhing <kam_ching@hotmail.com>
2022-01-13 09:54:59 +0800

    Update script to prompt deleting existing file before overwrite

--- mongodb-replica-set/create-key-file.sh
+++ mongodb-replica-set/create-key-file.sh
@@ -17,17 +17,29 @@ Usage () {
 	echo
 	echo "Usage: $SCRIPT_NAME"
 	echo "Options:"
-	echo " -f  [filename]               Keyfile output filename"
+	echo " -o  [filename]               Keyfile output filename"
 	echo " -n  [chars]                  Number of output characters"
 	echo " -h                           This help message"
 	echo
+}
+
+Prompt () {
+	local prompt=$1
+	if [ "$IS_PROMPT_PROCEED" == "yes" ]; then
+		echo "Y"
+		return
+	fi
+	local is_proceed
+	echo -e "$prompt [Y/n]" > /dev/tty
+	read is_proceed
+	echo "$is_proceed"
 }
 
 # Parse input argument(s)
 while [ "${1:0:1}" == "-" ]; do
 	OPT=${1:1:1}
 	case "$OPT" in
-	"f")
+	"o")
 		KEYFILE_FILENAME=$2
 		shift
 		;;
@@ -47,6 +59,15 @@ if [ "$#" -ne "$NUM_ARGS" ]; then
     echo "Invalid parameter!"
 	Usage
 	exit 1
+fi
+
+if [ -f "$KEYFILE_FILENAME" ]; then
+	is_overwrite=$(Prompt "Overwrite \"$KEYFILE_FILENAME\" file?")
+	if [ "$is_overwrite" != "Y" ]; then
+		echo "Abort creating keyfile."
+		exit
+	fi
+	rm -f $KEYFILE_FILENAME
 fi
 
 # docker exec to the container
