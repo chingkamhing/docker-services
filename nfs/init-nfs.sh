1f8ee548635cc363e0fd93b443b65e57cfc696ff
chingkamhing <kam_ching@hotmail.com>
2022-04-01 14:08:59 +0800

    Add init-nfs.sh script to create share directory for better management

---
+++ nfs/init-nfs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Script file to init nfs server. Typically, just create some directories to be shared by different applications.
+#
+
+# create directory "app" to be shared amoung applications
+mkdir /mnt/nfs/app
