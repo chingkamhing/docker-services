29d09fedb535bd5b30aafeb0e0498f753ef4fdd3
chingkamhing <kam_ching@hotmail.com>
2022-08-16 14:26:03 +0800

    Implement sub command "sub" to subscribe nats message

--- nats/cmd/subscribe.go
+++ nats/cmd/subscribe.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/nats-io/nats.go"
@@ -20,13 +23,10 @@ import (
 //
 
 var cmdSubscribe = &cobra.Command{
-	Use:   "sub",
+	Use:   "sub [subject]",
 	Short: "NATS subscribe related sub-commands",
-	Run: func(cmd *cobra.Command, args []string) {
-		_ = args
-		// default command: print usage
-		cmd.Usage()
-	},
+	Args:  cobra.ExactArgs(1),
+	Run:   runSubscribe,
 }
 
 var cmdSubscribeTestPull = &cobra.Command{
@@ -43,6 +43,7 @@ var cmdSubscribeTestQueue = &cobra.Command{
 	Run:   runSubscribeTestQueue,
 }
 
+var subIntervalCount int
 var numPubsub int
 var numQueue int
 var interval time.Duration
@@ -56,7 +57,45 @@ func init() {
 	cmdSubscribeTestQueue.Flags().DurationVar(&duration, "duration", 3*time.Second, "Publish duration")
 	cmdSubscribe.AddCommand(cmdSubscribeTestQueue)
 
+	cmdSubscribe.Flags().IntVar(&subIntervalCount, "interval", 1, "Subscribe print receive message interval count. Used to avoid excessive print message by skipping this count number.")
 	rootCmd.AddCommand(cmdSubscribe)
+}
+
+func runSubscribe(cmd *cobra.Command, args []string) {
+	config, err := configInit(cmd)
+	if err != nil {
+		log.Fatalf("configInit() error: %v", err)
+	}
+	subject := args[0]
+	// connect to NATS
+	natsConn, err := natsConnect(config)
+	if err != nil {
+		log.Fatalf("Connect() error: %v", err)
+	}
+	defer natsConn.Close()
+	// publish message
+	count := 0
+	_, err = natsConn.Subscribe(subject, func(msg *nats.Msg) {
+		if count%subIntervalCount == 0 {
+			log.Printf("[%v] %q", msg.Subject, string(msg.Data))
+		}
+		count++
+	})
+	if err != nil {
+		log.Fatalf("Subscribe() error: %v", err)
+	}
+	// wait receiving till break
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		fmt.Println()
+		done <- true
+	}()
+	log.Println("Awaiting NATS message...")
+	<-done
+	log.Println("Exit NATS receive message.")
 }
 
 // TestMessage is a message that can help test timings on jetstream
