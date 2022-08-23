73e73af900cb993e7fa269d0b374872302a85ca1
chingkamhing <kam_ching@hotmail.com>
2022-08-23 17:10:10 +0800

    Change back whoami port to 1443

--- traefik/mtls/test.sh
+++ traefik/mtls/test.sh
@@ -5,4 +5,4 @@
 # - i.e. add "127.0.0.1 kamching.freemyip.com whoami.kamching.freemyip.com nats.kamching.freemyip.com mqtt.kamching.freemyip.com ws.kamching.freemyip.com" in file /etc/hosts
 #
 
-curl -k --cacert whoami/cert/kamching.freemyip.com/ca.crt --cert whoami/cert/kamching.freemyip.com/client.crt --key whoami/cert/kamching.freemyip.com/client.key https://whoami.kamching.freemyip.com:4222
+curl -k --cacert whoami/cert/kamching.freemyip.com/ca.crt --cert whoami/cert/kamching.freemyip.com/client.crt --key whoami/cert/kamching.freemyip.com/client.key https://whoami.kamching.freemyip.com:1443
