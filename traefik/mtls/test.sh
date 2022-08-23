171b3b227958782dba562487a5d6c9744c53e19a
chingkamhing <kam_ching@hotmail.com>
2022-08-23 17:07:37 +0800

    Change whoami HostSNI to whoami.kamching.freemyip.com instead of reusing nats

--- traefik/mtls/test.sh
+++ traefik/mtls/test.sh
@@ -2,7 +2,7 @@
 #
 # Prerequitist:
 # - update /etc/hosts file so that the hostname matches the TLS SAN name
-# - i.e. add "127.0.0.1 kamching.freemyip.com nats.kamching.freemyip.com mqtt.kamching.freemyip.com ws.kamching.freemyip.com" in file /etc/hosts
+# - i.e. add "127.0.0.1 kamching.freemyip.com whoami.kamching.freemyip.com nats.kamching.freemyip.com mqtt.kamching.freemyip.com ws.kamching.freemyip.com" in file /etc/hosts
 #
 
-curl -k --cacert whoami/cert/kamching.freemyip.com/ca.crt --cert whoami/cert/kamching.freemyip.com/client.crt --key whoami/cert/kamching.freemyip.com/client.key https://nats.kamching.freemyip.com:4222
+curl -k --cacert whoami/cert/kamching.freemyip.com/ca.crt --cert whoami/cert/kamching.freemyip.com/client.crt --key whoami/cert/kamching.freemyip.com/client.key https://whoami.kamching.freemyip.com:4222
