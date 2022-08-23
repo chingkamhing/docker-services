0cb4bc82520a266ba9bf00444d39e882f9d10e9a
chingkamhing <kam_ching@hotmail.com>
2022-08-23 15:33:45 +0800

    Preliminarily have mtls passthrough working with traefik

--- traefik/mtls/test.sh
+++ traefik/mtls/test.sh
@@ -2,7 +2,7 @@
 #
 # Prerequitist:
 # - update /etc/hosts file so that the hostname matches the TLS SAN name
-# - i.e. add "127.0.0.1 my-domain.com" in file /etc/hosts
+# - i.e. add "127.0.0.1 my-domain.com nats.my-domain.com mqtt.my-domain.com ws.my-domain.com" in file /etc/hosts
 #
 
-curl http://my-domain.com:1443
+curl -k --cacert whoami/cert/my-domain.com/ca.crt --cert whoami/cert/my-domain.com/client.crt --key whoami/cert/my-domain.com/client.key https://nats.my-domain.com:1443
